bb55e10e745014c1e729b88b7c4594a47d24f27d
Tanvi975 <tanvi87917@gmail.com>
2026-09-05 08:42:37 +0530

    Setup Seed Journey project and add static scene

---
+++ script.js
@@ -0,0 +1,57 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const background = new Image();
+const sun = new Image();
+const cloud1 = new Image();
+const cloud2 = new Image();
+const tree = new Image();
+const flowers = new Image();
+const butterfly = new Image();
+
+background.src = "assets/background.png";
+sun.src = "assets/sun.png";
+
+cloud1.src = "assets/cloud.png";
+cloud2.src = "assets/cloud.png";
+tree.src = "assets/tree.png";
+flowers.src = "assets/flowers.png";
+butterfly.src = "assets/butterfly.png";
+
+let loadedImages = 0;
+
+function imageLoaded() {
+    loadedImages++;
+    if (loadedImages === 7) {
+        drawScene();
+    }
+}
+
+background.onload = imageLoaded;
+sun.onload = imageLoaded;
+cloud1.onload = imageLoaded;
+cloud2.onload = imageLoaded;
+tree.onload = imageLoaded;
+flowers.onload = imageLoaded;
+butterfly.onload = imageLoaded;
+
+function drawScene() {
+    ctx.drawImage(background, 0, 0, 1000, 600);
+    ctx.drawImage(sun, 50, 50, 130, 120);
+    ctx.drawImage(cloud1, 300, 70, 160, 96);
+    ctx.drawImage(cloud2, 600, 100, 180, 108);
+    ctx.drawImage(tree, 600, 255, 200, 233);
+    ctx.drawImage(flowers, 460, 350, 250, 125);
+    ctx.drawImage(butterfly, 480, 380, 30, 40);
+
+    ctx.fillStyle = "#725339";
+
+
+    ctx.beginPath();
+    ctx.ellipse(200, 510, 8, 14, 0, 0, Math.PI * 2);
+    ctx.fill();
+
+
+    ctx.strokeStyle = "#3E2418";
+    ctx.stroke();
+};
